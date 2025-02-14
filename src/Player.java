e12c0a25f209b126bc29ff8fafa992f0c5b29dd5
Chase Miller <a02283751@aggies.usu.edu>
2025-02-13 21:16:23 -0700

    center player in maze

--- src/Player.java
+++ src/Player.java
@@ -4,13 +4,12 @@ public class Player {
     private final Maze maze;
     private int row;
     private int col;
-    private float cellSize;
     private final int maxRow;
     private final int maxCol;
+    private final float cellSize;
     private final Rectangle playerRec;
-    final float PLAYER_LEFT = -0.4935f;
-    private float playerSize;
-    final float PLAYER_TOP = -0.4935f;
+    private final float playerLeft; //-0.4935f;
+    private final float playerTop; //-0.4935f;
 
     public Player(int row, int col, Maze maze) {
         this.row = row;
@@ -19,9 +18,11 @@ public class Player {
         this.maxCol = maze.getCols();
         this.maxRow = maze.getRows();
         this.cellSize = maze.getCellSize();
-        this.playerSize = this.cellSize * (30/48f);
-        float top = PLAYER_TOP + row * playerSize;
-        float left = PLAYER_LEFT + col * playerSize;
+        float playerSize = this.cellSize * (30 / 48f);
+        this.playerLeft = -0.5f + ((cellSize-playerSize)/2);
+        this.playerTop = -0.5f + ((cellSize-playerSize)/2);
+        float top = playerTop + row * playerSize;
+        float left = playerLeft + col * playerSize;
         this.playerRec = new Rectangle(left, top, playerSize, playerSize, 0.2f);
     }
 
@@ -33,28 +34,28 @@ public class Player {
     public void moveUp(float distance) {
         if (this.row > 0 && maze.wallNotExists(this.row, this.col, "up")) {
             this.row--;
-            playerRec.top = PLAYER_TOP + this.row  * cellSize;
+            playerRec.top = playerTop + this.row  * cellSize;
         }
     }
 
     public void moveDown(float distance) {
         if (this.row < this.maxRow - 1 && maze.wallNotExists(this.row, this.col, "down")) {
             this.row++;
-            playerRec.top = PLAYER_TOP + this.row  * cellSize;
+            playerRec.top = playerTop + this.row  * cellSize;
         }
     }
 
     public void moveLeft(float distance) {
         if (this.col > 0 && maze.wallNotExists(this.row, this.col, "left")) {
             this.col--;
-            playerRec.left = PLAYER_LEFT + this.col * cellSize;
+            playerRec.left = playerLeft + this.col * cellSize;
         }
     }
 
     public void moveRight(float distance) {
         if (this.col < this.maxCol - 1 && maze.wallNotExists(this.row, this.col, "right")) {
             this.col++;
-            playerRec.left = PLAYER_LEFT + this.col * cellSize;
+            playerRec.left = playerLeft + this.col * cellSize;
         }
     }
 }
